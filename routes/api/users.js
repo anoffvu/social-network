089a505a943e90f993d115434010ab1928be4592
anoffvu <anoffvu@gmail.com>
2019-08-27 01:50:02 -0500

    starting user registration
    goodnight

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const router = express.Router();
 
+// load user model
+const User = require('../../models/User')
+
 // @route   GET api/users/test
 // @desc    Tests users route
 // @access  Public
 router.get('/test', (req, res) => res.json({msg: "Users works"})
 );
+
+// @route   GET api/users/register
+// @desc    registers a user
+// @access  Public
+router.post('/register', (req, res) => {
+  User.findOne({ email: req.body.email })
+});
 
 module.exports = router;
