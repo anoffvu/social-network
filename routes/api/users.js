1795eed007bcafc34d9c77d4da4b6e1ae986a72b
anoffvu <anoffvu@gmail.com>
2019-09-06 09:54:13 -0400

    promise

--- routes/api/users.js
+++ routes/api/users.js
@@ -15,6 +15,18 @@ router.get('/test', (req, res) => res.json({msg: "Users works"})
 // @access  Public
 router.post('/register', (req, res) => {
   User.findOne({ email: req.body.email })
+    .then(user => {
+      if(user) {
+        return res.status(400).json({ email: 'Email already exists'});
+      } else {
+        const newUser = new User({
+          name: req.body.name,
+          email: req.body.email,
+          avatar: avatar,
+          password: req.body.password
+        });
+      }
+    })
 });
 
 module.exports = router;
